9806c77c76ae93366a4d401a4a01878ae4c892bc
Vítor Sousa <vitor.sousa@simposium.pt>
2023-10-13 21:46:33 +0000

    add header to map screen

--- front-end/screens/MapScreen.tsx
+++ front-end/screens/MapScreen.tsx
@@ -1,20 +1,57 @@
 import MapView from "react-native-maps";
 import { StyleSheet, View } from "react-native";
+import { makeStyles, Text, Button, useThemeMode } from "@rneui/themed";
+import { Header } from "@rneui/themed";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default () => {
+  const styles = useStyles();
+
   return (
-    <View style={styles.container}>
-      <MapView style={styles.map} />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Header
+          backgroundImageStyle={{}}
+          barStyle="default"
+          centerComponent={{
+            text: "MY TITLE",
+            style: { color: "#fff" },
+          }}
+          centerContainerStyle={{}}
+          containerStyle={{ width: "100%" }}
+          leftComponent={{ icon: "menu", color: "#fff" }}
+          leftContainerStyle={{}}
+          linearGradientProps={{}}
+          placement="center"
+          rightComponent={{ icon: "home", color: "#fff" }}
+          rightContainerStyle={{}}
+          statusBarProps={{}}
+        />
+        <MapView style={styles.map} />
+      </View>
+    </SafeAreaProvider>
   );
 };
 
-const styles = StyleSheet.create({
+const useStyles = makeStyles((theme) => ({
+  headerContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#397af8",
+    marginBottom: 20,
+    width: "100%",
+    paddingVertical: 15,
+  },
   container: {
+    backgroundColor: theme.colors.background,
     flex: 1,
   },
   map: {
     width: "100%",
     height: "100%",
   },
-});
+  header: {
+    height: "30%",
+    backgroundColor: "#fffff",
+  },
+}));
