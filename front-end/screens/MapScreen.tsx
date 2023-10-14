28e0f0c5d54aff758f521a1454bd1f9a6fa20a53
André Dias <2021114357@uac.pt>
2023-10-14 13:43:26 +0000

    Merge branch 'frontend/filters' of https://github.com/l33loo/stray-guardian into frontend/filters
    # Conflicts:
    #	front-end/screens/MapScreen.tsx

--- front-end/screens/MapScreen.tsx
+++ front-end/screens/MapScreen.tsx
@@ -1,10 +1,12 @@
-import { View } from "react-native";
-import { makeStyles } from "@rneui/themed";
-import { Header } from "@rneui/themed";
+import {TextInput, View} from "react-native";
+import {Icon, makeStyles, Dialog, Input} from "@rneui/themed";
+import { Header, Text} from "@rneui/themed";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Map } from "../components/Map";
 import Filters from "../components/Filters";
+import {Button} from "@rneui/base";
 import { useState } from "react";
+
 
 export default () => {
   const styles = useStyles();
@@ -19,21 +21,52 @@ export default () => {
     found: false,
     lost: false,
   });
+  const [name, setName] = useState('Stray Guardian');
+  const [token, setToken] = useState('');
+  const [showDialog, setShowDialog] = useState(false);
 
+
+  const handleSubmit = () => {
+    setShowDialog(false);
+    token === '' ? setName('Stray Guardian') : setName(token);
+  };
+
+  const dialogue = () => {
+    return (
+        <View>
+          <TextInput
+              placeholder='Please input your token'
+              value={token}
+              onChangeText={text => setToken(text)}
+          />
+          <Button title="Submit" onPress={handleSubmit}/>
+        </View>
+    )
+  };
   return (
+
     <SafeAreaProvider>
       <View style={styles.container}>
         <Header
           barStyle="default"
           centerContainerStyle={{}}
+          centerComponent={{ text: name, style: { color: '#fff' }}}
           containerStyle={{ width: "100%" }}
           leftContainerStyle={{}}
           linearGradientProps={{}}
           placement="center"
           rightComponent={{ icon: "add", color: "#fff" }}
+          leftComponent={<View style={styles.leftContainer}>
+            <Icon type={"material-community"} name={"key"} color="#FFFF" onPress={() => setShowDialog(true)} />
+        </View>}
         />
         <Filters filter={filter} setFilter={setFilter} />
         <Map filter={filter} />
+        {showDialog && ( // Conditionally render the dialog when showDialog is true
+            <Dialog>
+              {dialogue()}
+            </Dialog>
+        )}
       </View>
     </SafeAreaProvider>
   );
@@ -48,6 +81,7 @@ const useStyles = makeStyles((theme) => ({
     width: "100%",
     paddingVertical: 15,
   },
+
   container: {
     backgroundColor: theme.colors.background,
     flex: 1,
@@ -56,4 +90,13 @@ const useStyles = makeStyles((theme) => ({
     height: "30%",
     backgroundColor: "#fffff",
   },
+  leftContainer: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    paddingHorizontal: 5,
+  },
+  Icon: {
+    paddingLeft: 20,
+  }
 }));
