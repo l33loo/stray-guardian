88eb7457f07866064e15a4d91bdd3aaa07f223fa
Vítor Sousa <vitor.sousa@simposium.pt>
2023-10-14 02:50:11 +0000

    add geolocation

--- front-end/App.tsx
+++ front-end/App.tsx
@@ -1,6 +1,9 @@
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import MapScreen from "./screens/MapScreen";
-import { ThemeProvider, createTheme } from "@rneui/themed";
+import { ThemeProvider, createTheme, makeStyles } from "@rneui/themed";
+import * as Location from "expo-location";
+import { View, Text } from "react-native";
 
 const theme = createTheme({
   lightColors: {
@@ -19,6 +22,26 @@ const theme = createTheme({
 });
 
 export default function App() {
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const styles = useStyles();
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+    })();
+  }, []);
+
+  if (errorMsg != null) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>{errorMsg}</Text>
+      </View>
+    );
+  }
   return (
     <ThemeProvider theme={theme}>
       <MapScreen />
@@ -26,3 +49,16 @@ export default function App() {
     </ThemeProvider>
   );
 }
+
+const useStyles = makeStyles(() => ({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  paragraph: {
+    fontSize: 18,
+    textAlign: "center",
+  },
+}));
