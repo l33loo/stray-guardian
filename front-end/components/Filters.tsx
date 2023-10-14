e784bbfea630b88a786ca3139f844283bc33403a
André Dias <2021114357@uac.pt>
2023-10-14 06:05:33 +0000

    Add filter state

--- front-end/components/Filters.tsx
+++ front-end/components/Filters.tsx
@@ -5,7 +5,18 @@ import { useState } from "react";
 
 export default () => {
   const styles = useStyles();
-  const [filter, setFilter] = useState({});
+  const [filter, setFilter] = useState({
+    "dog" : false,
+    "cat" : false,
+    "bird" : false,
+    "bat" : false,
+    "cow" : false,
+    "rabbit" : false,
+    "unicorn" : false,
+  });
+
+
+
   return (
     <View style={styles.filterHeader}>
       <Button type="solid" style={styles.filterButton}>
@@ -21,54 +32,68 @@ export default () => {
       </Button>
       <ScrollView horizontal showsHorizontalScrollIndicator={false}>
         <View style={styles.filterHeader}>
-          <Button type="solid" style={styles.filterButton}>
-            <Icon name="dog" type="material-community" color="white" />
+          <Button type="solid" style={styles.filterButton} onPress={
+            () => setFilter({...filter, "dog": !filter["dog"]})
+          }>
+            <Icon name="dog" type="material-community" color={filter["dog"]?"white":"black"} />
           </Button>
-          <Button type="solid" style={styles.filterButton}>
+          <Button type="solid" style={styles.filterButton} onPress={
+            () => setFilter({...filter, "cat": !filter["cat"]})
+          }>
             <Icon
               name="cat"
               type="material-community"
-              color="white"
+              color={filter["cat"]?"white":"black"}
               style={styles.filterIcon}
             />
           </Button>
-          <Button type="solid" style={styles.filterButton}>
+          <Button type="solid" style={styles.filterButton} onPress={
+            () => setFilter({...filter, "bird": !filter["bird"]})
+          }>
             <Icon
               name="bird"
               type="material-community"
-              color="white"
+              color={filter["bird"]?"white":"black"}
               style={styles.filterIcon}
             />
           </Button>
-          <Button type="solid" style={styles.filterButton}>
+          <Button type="solid" style={styles.filterButton} onPress={
+            () => setFilter({...filter, "bat": !filter["bat"]})
+          }>
             <Icon
               name="bat"
               type="material-community"
-              color="white"
+              color={filter["bat"]?"white":"black"}
               style={styles.filterIcon}
             />
           </Button>
-          <Button type="solid" style={styles.filterButton}>
+          <Button type="solid" style={styles.filterButton} onPress={
+            () => setFilter({...filter, "cow": !filter["cow"]})
+          }>
             <Icon
               name="cow"
               type="material-community"
-              color="white"
+              color={filter["cow"]?"white":"black"}
               style={styles.filterIcon}
             />
           </Button>
-          <Button type="solid" style={styles.filterButton}>
+          <Button type="solid" style={styles.filterButton} onPress={
+            () => setFilter({...filter, "rabbit": !filter["rabbit"]})
+          }>
             <Icon
               name="rabbit"
               type="material-community"
-              color="white"
+              color={filter["rabbit"]?"white":"black"}
               style={styles.filterIcon}
             />
           </Button>
-          <Button type="solid" style={styles.filterButton}>
+          <Button type="solid" style={styles.filterButton} onPress={
+            () => setFilter({...filter, "unicorn": !filter["unicorn"]})
+          }>
             <Icon
               name="unicorn"
               type="material-community"
-              color="white"
+              color={filter["unicorn"]?"white":"black"}
               style={styles.filterIcon}
             />
           </Button>
@@ -80,7 +105,9 @@ export default () => {
 
 const useStyles = makeStyles((theme) => ({
   filterIcon: {},
-  filterButton: {},
+  filterButton: {
+    backgroundColor: "red",
+  },
   filterHeader: {
     backgroundColor: theme.colors.primary,
     display: "flex",
