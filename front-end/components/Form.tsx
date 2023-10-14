af918774ac85aea4a0c08e4768f3d1f31a42309d
Lila Karpowicz <lila@karpowicz.dev>
2023-10-14 11:39:16 +0000

    add submit form

--- front-end/components/Form.tsx
+++ front-end/components/Form.tsx
@@ -1,42 +1,52 @@
 import { useState } from 'react';
-import { Button, Image, Modal, Text, TextInput, View } from 'react-native';
+import { Button, Image, Modal, ScrollView, Text, TextInput, View } from 'react-native';
 import { Card, Icon, Tab } from "@rneui/base";
-import ImagePicker from 'expo-image-picker';
+import { makeStyles } from "@rneui/themed";
+import * as ImagePicker from 'expo-image-picker';
+import { setPin } from '../queries';
 
 export function Form() {
+    const styles = useStyles();
+
     const [isFormOpen, setIsFormOpen] = useState<boolean>(true);
     const [tabIndex, setTabIndex] = useState<number>(0);
     const [selectedImage, setSelectedImage] = useState<string | null>(null);
     const [observationsField, setObservationsField] = useState<string>('');
     const [emailField, setEmailField] = useState<string>('');
     const [phoneField, setPhoneField] = useState<string>('');
-    const [lastSeenDateField, setLastSeenDateField] = useState<string>('');
+    const [lastSeenDateField, setLastSeenDateField] = useState<string | null>(null);
 
     const pickImage = async () => {
-        let result = await ImagePicker.launchImageLibraryAsync({
-            allowsEditing: true,
-            quality: 1,
-        });
-    
-        if (!result.canceled) {
-            setSelectedImage(result.assets[0].uri);
-            // setShowAppOptions(true);
-        } else {
-            alert('You did not select any image.');
-        }
+        const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+        if (granted) {
+            let result = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                quality: 1,
+            });
+        
+            if (!result.canceled) {
+                setSelectedImage(result.assets[0].uri);
+                // setShowAppOptions(true);
+            } else {
+                alert('You did not select any image.');
+            }
+        };
     };
     
     const takeImage = async () => {
-        let result = await ImagePicker.launchCameraAsync({
-            allowsEditing: true,
-            quality: 1,
-        });
-    
-        if (!result.canceled) {
-            setSelectedImage(result.assets[0].uri);
-            // setShowAppOptions(true);
-        } else {
-            alert('You did not select any image.');
+        const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+        if (granted) {
+            let result = await ImagePicker.launchCameraAsync({
+                // allowsEditing: true,
+                quality: 1,
+            });
+        
+            if (!result.canceled) {
+                setSelectedImage(result.assets[0].uri);
+                // setShowAppOptions(true);
+            } else {
+                alert('You did not select any image.');
+            }
         }
     };
 
@@ -45,7 +55,7 @@ export function Form() {
             visible={isFormOpen}
             animationType="slide"
             onRequestClose={() => setIsFormOpen(false)}
-            style={{display: 'flex'}}
+            // style={{display: 'flex'}}
         >
             <Icon 
                 name='close'
@@ -71,57 +81,86 @@ export function Form() {
                 </Tab>
             </Card.Title>
             <Card.Divider/>
-            <View>
-                <Button title="Pick an image from camera roll" onPress={pickImage} />
-                {selectedImage && <Image source={{ uri: selectedImage }} style={{ width: 200, height: 200 }} />}
-            </View>
-            <View>
-                <View style={{position:"relative", alignItems:"center"}}>
-                    {/* <Image
-                        style={{ height: 300, width: 300 }}
-                        resizeMode="contain"
-                        source={{ uri: modalContent ? modalContent.photoUrl : '' }}
-                    /> */}
+            <ScrollView>
+                <View>
+                    <Button title="Pick an image from camera roll" onPress={pickImage} />
+                    <Button title="Take an image" onPress={takeImage} />
                 </View>
-                <View style={{padding: 20}}>
-                    { tabIndex === 0 &&
-                        (<>
-                            <Text>Last seen on:</Text>
+                <View>
+                    <View style={{position:"relative", alignItems:"center"}}>
+                        {selectedImage &&
+                            <Image 
+                                source={{ uri: selectedImage }}
+                                style={{ width: 300, height: 600 }}
+                                resizeMode="contain"
+                            />
+                        }
+                    </View>
+                    <View style={{padding: 20}}>
+                        { tabIndex === 0 &&
+                            (<>
+                                <Text>Last seen on:</Text>
                                 <TextInput
-                                // style={styles.input}
+                                style={styles.input}
                                 onChangeText={setLastSeenDateField}
                                 value={lastSeenDateField}
-                            />
-                        </>
-                        )
-                    }
+                                />
+                            </>
+                            )
+                        }
 
-                    <Text>Observations:</Text>
-                    <TextInput
-                        // style={styles.input}
-                        onChangeText={setObservationsField}
-                        value={observationsField}
-                    />
-                    <Text>Email:</Text>
-                    <TextInput
-                        // style={styles.input}
-                        onChangeText={setEmailField}
-                        value={emailField}
-                    />
-                    <Text>Phone:</Text>
-                    <TextInput
-                        // style={styles.input}
-                        onChangeText={setPhoneField}
-                        value={phoneField}
-                    />
+                        <Text>Observations:</Text>
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={setObservationsField}
+                            value={observationsField}
+                        />
+                        <Text>Email:</Text>
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={setEmailField}
+                            value={emailField}
+                        />
+                        <Text>Phone:</Text>
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={setPhoneField}
+                            value={phoneField}
+                        />
+                    </View>
                 </View>
-            </View>
-            <Button
-                // onPress={onPressLearnMore}
-                title="Submit"
-                color="#841584"
-            />
+                <Button
+                // const [selectedImage, setSelectedImage] = useState<string | null>(null);
+                // const [observationsField, setObservationsField] = useState<string>('');
+                // const [emailField, setEmailField] = useState<string>('');
+                // const [phoneField, setPhoneField] = useState<string>('');
+                // const [lastSeenDateField, setLastSeenDateField] = useState<string>('');
+            
+                    onPress={() => setPin({
+                        photoUrl: selectedImage,
+                        type: tabIndex === 0 ? 'lost' : 'found',
+                        color: "#000",
+                        status: status,
+                        lastSeen: new Date(lastSeenDateField),
+                        lat: lat,
+                        lon: lon,
+                        phone: phone,
+                        email: email,
+                        observations: observations,
+                    })}
+                    title="Submit"
+                    color="#841584"
+                />
+            </ScrollView>
         </Modal>
-    )
+    );
 }
 
+const useStyles = makeStyles((theme) => ({
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+}));
--- front-end/queries.ts
+++ front-end/queries.ts
@@ -7,3 +7,25 @@ export async function getPins() {
     return [];
   }
 }
+
+export async function setPin(data: any) {
+  try {
+    const response = await fetch("http://10.10.11.36:8000/api/stray", {
+      method: "POST",
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  } catch(e) {
+    console.log(e);
+    return null;
+  }
+}
