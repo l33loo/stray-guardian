cffc8df61c9811039cc02eb79ed4d94a1b92f396
Lila Karpowicz <lila@karpowicz.dev>
2023-10-14 03:35:39 +0000

    put coordinates for Terceira

--- front-end/queries.ts
+++ front-end/queries.ts
@@ -10,7 +10,7 @@ export function getPins() {
             date: '12344',
             token: '243',
             photoUrl: 'https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg',
-            coordinate: {latitude: 37.748976654581526, longitude: -25.66777811961261},
+            coordinate: {latitude: 38.662116, longitude: -27.221159},
         },
         {
             animalType: 'dog',
@@ -22,7 +22,7 @@ export function getPins() {
             token: '679',
             photoUrl: 'https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80',
             color: 'brown',
-            coordinate: {latitude: 37.750033, longitude: -25.669765},
+            coordinate: {latitude: 38.659385, longitude: -27.228497},
         },
         {
             animalType: 'seagull',
@@ -34,7 +34,7 @@ export function getPins() {
             token: '356',
             photoUrl: 'https://images.unsplash.com/photo-1531482922603-0a5dfa476444?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
             color: 'white',
-            coordinate: {latitude: 37.747976, longitude: -25.653857},
+            coordinate: {latitude: 38.658832, longitude: -27.219614},
         },
     ];
 
